9f6edba3e2bcdf17a052e4074e7d14737e0fa622
Douglas <douglasclaudino10.dc@gmail.com>
2023-09-13 19:51:14 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import { Login } from './Pages/Login'
+import { Register } from './Pages/Register'
+
+function App() {
+
+  return (
+      <div className='text-white h-[100vh] w-[100vw] flex justify-center items-center bg-cover' style={{"backgroundImage": "url('../src/assets/homebg.jpg')"}}>
+        <Routes>
+          <Route path='login' element={<Login/>}></Route>
+          <Route path='register' element={<Register/>}></Route>
+        </Routes>
+      </div>
+  )
+}
+
+export default App
