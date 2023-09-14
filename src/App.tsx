5018e2675a594eb75bafd9c32784882c6a8f9c6c
Douglas <douglasclaudino10.dc@gmail.com>
2023-09-13 21:17:53 -0300

    add login and register routes

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App() {
   return (
       <div className='text-white h-[100vh] w-[100vw] flex justify-center items-center bg-cover' style={{"backgroundImage": "url('../src/assets/homebg.jpg')"}}>
         <Routes>
-          <Route path='login' element={<Login/>}></Route>
-          <Route path='register' element={<Register/>}></Route>
+          <Route path='login' element={<Login/>}/>
+          <Route path='register' element={<Register/>}/>
         </Routes>
       </div>
   )
